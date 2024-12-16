3bdaa037359df199e02156fce82e54163e0117bc
akhmelev <akhmelev@gmail.com>
2024-12-16 20:57:07 +0300

    ADD AuthorizationFilter

--- src/main/java/com/javarush/khmelov/filter/ErrorCleanerFilter.java
+++ src/main/java/com/javarush/khmelov/filter/ErrorCleanerFilter.java
@@ -5,7 +5,6 @@ import com.javarush.khmelov.util.Key;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,21 +12,14 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter({
-        Go.INDEX, Go.HOME,
-        Go.SIGNUP, Go.LOGIN, Go.LOGOUT,
-        Go.LIST_USER, Go.PROFILE, Go.EDIT_USER,
-        Go.CREATE, Go.QUEST, Go.PLAY_GAME
-})
-public class ErrorCleaner extends HttpFilter {
+@WebFilter({Go.INDEX, Go.HOME, Go.SIGNUP, Go.LOGIN, Go.LOGOUT, Go.LIST_USER, Go.PROFILE, Go.EDIT_USER, Go.CREATE, Go.QUEST, Go.PLAY_GAME})
+public class ErrorCleanerFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-       //
         chain.doFilter(req, res);
-        //
         HttpSession session = req.getSession(false);
-        if (req.getMethod().equals("GET") && session!=null) {
+        if (req.getMethod().equals("GET") && session != null) {
             session.removeAttribute(Key.ERROR_MESSAGE);
         }
     }
