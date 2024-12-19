e7e9854814fb9b637e526e7563fd7e81273cc37d
akhmelev <akhmelev@gmail.com>
2024-12-19 17:39:37 +0300

    ADD BaseIT+EditUser+Home

--- src/test/java/com/javarush/khmelov/cmd/LoginIT.java
+++ src/test/java/com/javarush/khmelov/cmd/LoginIT.java
@@ -1,36 +1,17 @@
 package com.javarush.khmelov.cmd;
 
-import com.javarush.khmelov.config.Config;
 import com.javarush.khmelov.config.Winter;
 import com.javarush.khmelov.entity.User;
 import com.javarush.khmelov.util.Key;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
-class LoginIT {
+class LoginIT extends BaseIT {
 
-    private Login login;
-    private HttpServletRequest request;
-    private HttpServletResponse response;
-    private HttpSession session;
-
-    @BeforeEach
-    void init() {
-        Config config = Winter.find(Config.class);
-        config.fillEmptyRepository();
-        login = Winter.find(Login.class);
-        request = mock(HttpServletRequest.class);
-        response = mock(HttpServletResponse.class);
-        session = mock(HttpSession.class);
-        when(request.getSession()).thenReturn(session);
-    }
+    final Login login = Winter.find(Login.class);
 
     @Test
     @DisplayName("When login admin then redirect to profile")
