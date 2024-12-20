2f0e88d4a544ae24f198a29f56fbfb2800001729
akhmelev <akhmelev@gmail.com>
2024-12-20 03:27:03 +0300

    ADD Tomcat embedded (remove war)

---
+++ src/main/java/com/javarush/khmelov/Application.java
@@ -0,0 +1,11 @@
+package com.javarush.khmelov;
+
+import com.javarush.khmelov.server.Tom;
+
+public class Application {
+    public static void main(String[] args) {
+        Tom.run();
+    }
+}
+
+
